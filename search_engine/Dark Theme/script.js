49a390637f58393e5832411a91b9245068fc6f8d
kunalbhatia <kunal18042002@gmail.com>
2019-07-16 15:44:34 +0530

    Add files via upload

---
+++ search_engine/Dark Theme/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('#rit').click(function() {
+        $('.hor2').toggleClass('bgColor');
+        $('#mainText').toggleClass('white');
+
+        $('#blah').find('.mainImg').toggle();
+    });
+
+    
+});
